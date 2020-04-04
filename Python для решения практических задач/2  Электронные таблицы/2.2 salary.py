90a12c72244601bc345aa65dbaacdf9fba8f9bcd
Stanislav Semenov <semenov_sd@bk.ru>
2020-04-04 11:53:20 +0300

    Initial commit of the project

--- Python для решения практических задач/2  Электронные таблицы/2.2 salary.py
+++ Python для решения практических задач/2  Электронные таблицы/2.2 salary.py
@@ -1,12 +1,27 @@
 import xlrd
 
-wb = xlrd.open_workbook('salaries.xlsx')
-sheet_names = wb.sheet_names()
-sh = wb.sheet_by_name(sheet_names[0])
+wb = xlrd.open_workbook('salaries.xlsx')  # read xlsx file
+sheet = wb.sheet_by_index(0)  # get sheet by index
 
-print(sh.row_values(2)[2])
+# dict = [x for x in sheet.row_values(1, start_colx = 1)]
 
+d2 = {}
+for x in range(1, sheet.nrows):
+    for y in range(1):
+        d2[sheet.cell_value(x, 0)] = {sheet.cell_value(0, y): sheet.cell_value(x, y)}
+print(d2)
+# num_rows = sheet.nrows  # row len
+# num_col = sheet.ncols  # colum len
 
+# Выведите название региона с самой высокой медианной зарплатой
+# через пробел, название профессии с самой высокой средней зарплатой по всем регионам.
+
+# print(dict)  # get cell values
+
+# for rownum in range(sheet.nrows):
+#     row = sheet.row_values(rownum)
+#     for cel in row:
+#         print(cel)
 
 
 # nmin = sh.row_values(6)[2]
